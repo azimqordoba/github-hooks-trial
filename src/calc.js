1538f35222feece665285c769452e3f3c780cc1b
Azim Saiyed <azim@qordoba.com>
2020-11-19 14:11:33 -0600

    feat: adds addAll func

--- src/calc.js
+++ src/calc.js
@@ -1,3 +1,5 @@
 export const square = (value) => value * value;
 
 export const multiply = (arr) => arr.reduce((result, val) => result * val, 1);
+
+export const addAll = (arr) => arr.reduce((result, val) => result + val, 1);
