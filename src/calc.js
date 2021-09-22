2fe35c455ed2f03ae4ffaf91d805067804c6c024
azimqordoba <53835880+azimqordoba@users.noreply.github.com>
2021-09-22 16:01:03 -0500

    Update calc.js

--- src/calc.js
+++ src/calc.js
@@ -3,3 +3,7 @@ export const square = (value) => value * value;
 export const multiply = (arr) => arr.reduce((result, val) => result * val, 1);
 
 export const addAll = (arr) => arr.reduce((result, val) => result + val, 1);
+
+function azim () { 
+  const a;
+}
